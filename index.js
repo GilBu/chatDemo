ffcf67ae18c6f86f277366f477c8896927c880b6
Gilbert <solknightx@gmail.com>
2018-01-17 21:27:02 -0800

    added connect and disconnect notifications

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  conMsg = ('a user has connected');
+  io.emit('connect message', conMsg);
+  socket.on('disconnect', function(conMsg){
+    conMsg = ('a user has disconnected');
+    io.emit('connect message', conMsg);
+  })
 });
 
 http.listen(4000, function(){
